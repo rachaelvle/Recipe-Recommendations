b4e6b5953210b86b5e9feb32ce8d5ee5bc1b8905
DNGUYEN168 <107276510+DNGUYEN168@users.noreply.github.com>
2026-02-26 23:17:08 -0800

    updates to login

--- app/auth/Login.tsx
+++ app/auth/Login.tsx
@@ -1,48 +1,53 @@
+import { api } from "@/lib/api";
 import { styles } from "@/styles/SimpleStyleSheet";
 import { router } from "expo-router";
 import React, { useState } from "react";
 import { Pressable, Text, TextInput, View } from "react-native";
-import { loadUser } from "../../lib/jsonCommands";
-// create a password checking with a simple password / email hardcoded list
+import { StoreCurrentUserID } from "../../Utils/jsonCommands";
+// create a password checking with a simple password / email hardcoded list 
 
 export default function Login() {
-  // store user input to be later checked for login
-  const [email, setEmail] = useState("");
+
+  // store user input to be later checked for login 
+  const [UserName, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  const handleSubmit = async () => {
-    // takes user input and handles email and password
-    if (!email || !password) {
+
+  const handleSubmit = async () => { // takes user input and handles email and password 
+    if (!UserName || !password) 
+    {
       setError("Please enter email and password");
       return;
-    } // no input
-
-    const user = await loadUser(email); // pass in the provided email
-    if (!user || user.password != password) {
-      setError("Invalid Credentials");
-      return;
-    }
+    } // no input 
 
     setError(""); // clear error
-
     // just for testing right now will make actual account checking maybe :3
-    if (email == user.email && password == user.password) {
-      router.push("/(tabs)" as any); // CHANGE LATER WHEN FINALIZING
-    }
-  };
+    try {
+      const loginResponse = await api.login(UserName, password); // check database for user login
+      StoreCurrentUserID(loginResponse.user.id) // STORE USER ID TO PASS ONTO NEXT PAGE ON THE STACK
 
-  return (
+      // *************************************** USING FOR TESTING RIGHT NOW CHANGE TO SOMETHING ELSE LATER **************************************************
+      router.push('/UserProfileCreation/ShowUserProfile') // go to home or something 
+
+    } catch (error) {
+      console.error('Login Failed', error); // debugging 
+      setError("Invalid Credentials"); // visuals
+    } 
+
+  }
+
+    return (
     <View style={styles.container}>
       <Text style={styles.HeaderText}>Login</Text>
-
+      
       <TextInput
-        placeholder="Email"
+        placeholder="Username"
         placeholderTextColor="#aaa"
         style={styles.input}
-        value={email}
+        value={UserName}
         autoCapitalize="none"
-        onChangeText={setEmail} // sets email when user sets email
+        onChangeText={setUserName} // sets email when user sets email 
         keyboardType="email-address"
       />
 
@@ -52,14 +57,23 @@ export default function Login() {
         style={styles.input}
         value={password}
         onChangeText={setPassword} // sets password when user inputs in text box
-        secureTextEntry // password dots
+        secureTextEntry // password dots 
       />
 
-      {error ? <Text style={styles.error}>{error}</Text> : null}
-
+      {error ? <Text style={styles.error}>{error}</Text> : null} 
+      
       <Pressable style={styles.YesButton} onPress={handleSubmit}>
         <Text style={styles.Text}>Sign In</Text>
       </Pressable>
+
+      <View style={{ marginTop: 30 }}>
+        <Pressable onPress={() => router.push('/UserProfileCreation/CreateUserProfile')}>
+          <Text style={{ textAlign: "center", color: "black", fontWeight: "600" }}>
+            No Account? Sign Up
+          </Text>
+        </Pressable>
+      </View>
+      
     </View>
   );
 }
